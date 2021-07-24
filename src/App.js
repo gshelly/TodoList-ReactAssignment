3a6151bea19538bc12e0f7cf37a0bf56637cbe27
gshelly <gshelly1402@gmail.com>
2021-07-23 19:28:46 -0700

    Created a todo list with Add Update Delete Functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import TodoList from './Components/TodoList';
 import './App.css';
+import { useState } from 'react';
+import UpdateDelete from './Components/UpdateDeleteTodoList';
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList todos={todos} setTodos={setTodos} />
+      <UpdateDelete todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
